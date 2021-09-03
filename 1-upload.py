8d6042f888823dcff9d0610e004a2fd8f8029297
seanth <seanth@gmail.com>
2021-09-03 15:36:33 -0500

    Ability to remove ambient audio
    -Added ability to remove ambient room noise
    -After making edits to audio and copying it to intermediate folder, raw audio is moved to "processed" folder
    -After uploading video, temp video is moved to "processed" folder

--- 1-upload.py
+++ 1-upload.py
@@ -11,12 +11,17 @@ import fileUtils
 #The httplib module has been renamed to http.client in Python 3.
 #import httplib
 import http.client
-from dateutil.relativedelta import relativedelta #python3 -m pip install python-dateutil
 
-import httplib2 #python3 -m pip install httplib2
+
+
 import os
 import random
 import time
+
+from dateutil.relativedelta import relativedelta #python3 -m pip install python-dateutil
+import httplib2 #python3 -m pip install httplib2
+
+#import sox #python3 -m pip install sox
 
 from oauth2client.file import Storage # python3 -m pip install oauth2client
 from oauth2client import client 
@@ -211,11 +216,14 @@ def resumable_upload(request, theVideoFileName):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Who wants some popcorn?')
-    parser.add_argument('--audio', metavar='', dest='boostAudio', default=False, required=False, help='boost the audio? (False)' )
+    parser.add_argument('--boost', metavar='', dest='boostAudio', default=False, required=False, help='boost the audio? (False)' )
+    parser.add_argument('--rmnoise', metavar='', dest='rmnoise', default=False, required=False, help='remove room noise using file (False)' )
     parser.add_argument('--video', metavar='', dest='makeVideo', default=True, required=False, help='make a simple video with the provided||boosted audio? (True)' )
     parser.add_argument('--upload', metavar='', dest='uploadVideo', default=True, required=False, help='upload simple video? (True)' )
     parser.add_argument('--audioIn', metavar='', dest='theAudioDirIn', default='input/raw_audio', required=False, help='path to folder containing mp3s' )
-    parser.add_argument('--audioOut', metavar='', dest='theAudioDirOut', default='intermediate/processed_audio', required=False, help='path to folder containing mp3s')
+    parser.add_argument('--ambientIn', metavar='', dest='theAmbientDirIn', default='input/raw_audio/ambient', required=False, help='path to folder containing mp3s' )
+    parser.add_argument('--ambientOut', metavar='', dest='theAmbientDirOut', default='intermediate/audio_profiles', required=False, help='path to folder containing mp3s' )
+    parser.add_argument('--audioOut', metavar='', dest='theAudioDirOut', default='intermediate/processed_audio', required=False, help='path to folder containing mp3s of ambient room noise')
     parser.add_argument('--image', metavar='', dest='theImagePath', default='lecture-daemon_data/testPattern.png', required=False, help='path to still image to use for video')
     parser.add_argument('--videoOut', metavar='', dest='theRawVideoDir', default='intermediate/temp_video', required=False, help='path to the folder videos will be written to')
     parser.add_argument('--category', default='27', help='Numeric video category. ' + 'See https://developers.google.com/youtube/v3/docs/videoCategories/list')
@@ -241,14 +249,23 @@ if __name__ == '__main__':
     ###########################################################################
     #do some checks to see whether files and folders needed actually exist
     args.theAudioDirIn = fileUtils.pathExistsMake(args.theAudioDirIn, True)
+    if args.rmnoise:
+        args.theAmbientDirIn = fileUtils.pathExistsMake(args.theAmbientDirIn, True)
+    theProcessedRawAudioDir = os.path.join(args.theAudioDirIn,"processed")
+    theProcessedRawAudioDir = fileUtils.pathExistsMake(theProcessedRawAudioDir, True)
     fileUtils.pathExistsMake("intermediate", True)
     args.theAudioDirOut = fileUtils.pathExistsMake(args.theAudioDirOut, True)
+    args.theAmbientDirOut = fileUtils.pathExistsMake(args.theAmbientDirOut, True)
     args.theRawVideoDir = fileUtils.pathExistsMake(args.theRawVideoDir, True)
+    theProcessedTempVideo = os.path.join(args.theRawVideoDir,"processed")
+    theProcessedTempVideo = fileUtils.pathExistsMake(theProcessedTempVideo, True)
     args.theImagePath = fileUtils.pathExistsMake(args.theImagePath,False)
     ###########################################################################
 
     ###########################################################################
     #check and see if boolean things are boolean
+    if isinstance(args.rmnoise, str):
+        args.rmnoise = booleanCheck(args.rmnoise)
     if isinstance(args.boostAudio, str):
         args.boostAudio = booleanCheck(args.boostAudio)
     if isinstance(args.makeVideo, str):
@@ -261,7 +278,31 @@ if __name__ == '__main__':
         print("       Exiting.")
         sys.exit()
 
-    if(args.boostAudio==True):
+
+    if(args.rmnoise==True):
+        tempVar = os.path.join(args.theAudioDirIn, "*.mp3")
+        for theFileName in glob.glob(tempVar):
+            theAudioFileName = os.path.basename(theFileName)
+            theAudioFileName = os.path.splitext(theAudioFileName)[0]
+            theAudioIn = os.path.join(args.theAudioDirIn,theAudioFileName+".mp3")
+            theAudioOutPath = os.path.join(args.theAudioDirOut,theAudioFileName+".mp3")
+            theAmbientIn = os.path.join(args.theAmbientDirIn,theAudioFileName+"-amb.mp3")
+            theAmbientOut = os.path.join(args.theAmbientDirOut,theAudioFileName+".prof")
+            theProcessedRawAudio = os.path.join(theProcessedRawAudioDir,theAudioFileName+".mp3")
+            fileUtils.pathExists(theAmbientIn)
+
+            theCommand = "sox '%s' -n noiseprof '%s'" % (theAmbientIn, theAmbientOut)
+            print("         Generating ambient audio profile...")
+            os.system(theCommand)
+
+            theCommand = "sox '%s' '%s' noisered '%s' 0.01" % (theAudioIn, theAudioOutPath, theAmbientOut)
+            print("         Making audio with reduced ambient noise...")
+            os.system(theCommand)
+
+            print("         Clean up of raw audio file...")
+            theCommand = "mv '%s' '%s'" % (theAudioIn, theProcessedRawAudio)
+            os.system(theCommand)
+    elif(args.boostAudio==True):
         tempVar = os.path.join(args.theAudioDirIn, "*.mp3")
         for theFileName in glob.glob(tempVar):
             theAudioFileName = os.path.basename(theFileName)
@@ -269,7 +310,7 @@ if __name__ == '__main__':
             
             theAudioOutPath = os.path.join(args.theAudioDirOut,theAudioFileName+".mp3")
             print("\n     Audio file '%s' found" % theFileName)
-            print("       Sending file to ffmpeg to process audio.....")
+            print("       Sending file to ffmpeg to process audio...")
             theCommand = "ffmpeg -i '%s' -ac 1 -filter:a loudnorm -b:a 128k '%s' >/dev/null" % (theFileName, theAudioOutPath)
             #print(theCommand)
             os.system(theCommand)
@@ -324,6 +365,13 @@ if __name__ == '__main__':
 
             try:
                 initialize_upload(youtube, args, theVideoFileName)
+                print("         Clean up of temp file...")
+                theVideoFileName = os.path.basename(theFileName)
+                theVideoFileName = os.path.splitext(theAudioFileName)[0]
+                theProcessedTmpVideo = os.path.join(args.theRawVideoDir,theVideoFileName+".mp4")
+
+                theCommand = "mv %s %s" % (theFileName, theProcessedTmpVideo)
+                os.system(theCommand)
             except HttpError as e:
                 print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
 
